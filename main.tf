4026434dc0aee4a99bba44fbbe6387e076a301d9
Ajay Rawat <ajayrawat111@gmail.com>
2024-06-27 22:13:21 +0530

    updated

--- main.tf
+++ main.tf
@@ -17,3 +17,12 @@ resource "azurerm_resource_group" "tf-rg" {
   name     = "jenkins-rg"
   location = "West Europe"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG-Storage"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "addsstoragescript"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "terraform-tffiles"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
